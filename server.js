67b6e93f2004d01b01dcddf259c1647213861588
effyn <effyn@tutanota.com>
2022-01-02 21:16:58 -0800

    Screw env on remote actually

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
 const fs = require('fs')
 const https = require('https')
 const WSServer = require('ws').Server
 
-const https_key = fs.readFileSync(process.env.CERT_LOCATION + 'privkey.pem', 'utf8')
-const https_cert = fs.readFileSync(process.env.CERT_LOCATION + 'fullchain.pem', 'utf8')
+const https_key = fs.readFileSync('./privkey.pem', 'utf8')
+const https_cert = fs.readFileSync('./fullchain.pem', 'utf8')
 const https_server_options = { key: https_key, certificate: https_cert }
 const https_server = https.createServer(https_server_options)
 https_server.listen(8080)
