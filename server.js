9ea4a645476bc8633cff47dfe0b64337af324872
effyn <effyn@tutanota.com>
2022-01-02 20:40:13 -0800

    HTTPS for server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
+const fs = require('fs')
+const https = require('https')
 const WSServer = require('ws').Server
+
+const https_key = fs.readFileSync(process.env.HOST_LOCATION + 'privkey.pem', 'utf8')
+const https_cert = fs.readFileSync(process.env.HOST_LOCATION + 'fullchain.pem', 'utf8')
+const https_server_options = { key: https_key, certificate: https_cert }
+const https_server = https.createServer(https_server_options)
+https_server.listen(8080)
+
 const wss = new WSServer({
-  host: '0.0.0.0',
-  port: 8080
+  server: https_server  
 })
+
 let all = []
 wss.on('connection', sock => {
   all.push(sock)
