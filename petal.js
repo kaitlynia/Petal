8768fc87ca5df1eeec205935e044a9e03b242f27
effyn <effyn@tutanota.com>
2022-01-02 22:09:41 -0800

    Typo again

--- petal.js
+++ petal.js
@@ -110,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     messages.innerHTML += `<p class="msg">${contents}</p>`
 
-    if (scrollPos + userSettings.scrollThreshold >= scrollHeight) {
+    if (messages.scrollTop + userSettings.scrollThreshold >= scrollHeight) {
       messages.scrollTop = messages.scrollHeight
     }
   }
