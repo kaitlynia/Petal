3b66fa9ac72759867e5e98df551807f7453742ef
effyn <effyn@tutanota.com>
2022-01-02 20:51:22 -0800

    Typo

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const https = require('https')
 const WSServer = require('ws').Server
 
-const https_key = fs.readFileSync(process.env.HOST_LOCATION + 'privkey.pem', 'utf8')
-const https_cert = fs.readFileSync(process.env.HOST_LOCATION + 'fullchain.pem', 'utf8')
+const https_key = fs.readFileSync(process.env.CERT_LOCATION + 'privkey.pem', 'utf8')
+const https_cert = fs.readFileSync(process.env.CERT_LOCATION + 'fullchain.pem', 'utf8')
 const https_server_options = { key: https_key, certificate: https_cert }
 const https_server = https.createServer(https_server_options)
 https_server.listen(8080)
